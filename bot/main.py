ee7d52388b3a32a422dae2fb331c526609bb3fc1
Rochelle Bartilet <71032863+rbartilet@users.noreply.github.com>
2022-01-16 13:25:42 -0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -49,4 +49,4 @@ async def on_message(message):
     elif 'sammus' in message.content:
       await message.channel.send('Sorry, I do not know how to respond to that.')
 
-client.run('OTMwMTUzMTU5NDI5NTk1MTk3.Ydxuog.E5w59HWWJuytzTSPt-bu9SFFqWU')
+client.run('os.getenv('TOKEN')')
